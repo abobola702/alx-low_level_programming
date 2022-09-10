b71c601aed89854fea4ea147c16d23fec6b1ce90
abobola702 <abobola702@gmail.com>
2022-09-10 12:51:56 -0700

    Printing using stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Printing using stderr
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (0);
+}

f252c31bd1a5fe1fd3e5158937bcdabf23d4f4d1
abobola702 <abobola702@gmail.com>
2022-09-14 22:11:43 -0700

    aditioin

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adding two integers
+ * @num1: first integer to be added
+ * @num2: second integer to be added
+ * Return: Result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+

e0b12e11566177c27a1e10c4fe123099b267f1d2
abobola702 <abobola702@gmail.com>
2022-09-14 19:44:07 -0700

    base ten numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - base ten numbers
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		printf("%d", c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}

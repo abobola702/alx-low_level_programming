1e1e260a1ba99e5a01631952aa4389769a90ef54
abobola702 <abobola702@gmail.com>
2022-09-19 06:08:30 -0700

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute value of integer
+ * @j: integer to the worked on
+ * Return: Absolute integer value
+ */
+
+int _abs(int j)
+{
+	return (j);
+}

d851e5b0114467073fe02e1697ad763a36480544
abobola702 <abobola702@gmail.com>
2022-09-20 06:28:36 -0700

    Multiplication table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Multiplication table of 9
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			a = j * i;
+			if (a < 10)
+			{
+				_putchar(' ');
+				_putchar((a) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

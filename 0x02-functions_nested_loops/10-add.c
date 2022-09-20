468233100125dba3385cf5d53328cc0122b256d7
abobola702 <abobola702@gmail.com>
2022-09-20 07:24:50 -0700

    Addition of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integer together
+ * @num1: First integer to be added
+ * @num2: Second integer to be added
+ * Result: result of the addition
+ */
+
+int add(int num1, int num2)
+{
+	int res;
+
+	res = num1 + num2;
+	return (res);
+}

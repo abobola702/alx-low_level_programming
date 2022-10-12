0fd59d446e74fa41ece658f397bd4c88cf934295
abobola702 <abobola702@gmail.com>
2022-10-12 06:04:01 -0700

    Debugging first task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,13 @@
- #include "main.h"
-
+#include <stdio.h>
+#include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
-  * Return: 0
-*/
+ *main - testing positive or negative function
+ * Return: Always  0 (Success)
+ */
 
-int main(void)
+int main (void)
 {
-	int i;
-
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
